3a773e188420b9b18d2178a4cae502c47f8a775b
zcr003 <zramsey1@cnm.edu>
2021-10-05 11:05:51 -0600

    Used thread to fetch/log animals.

--- app/src/main/java/edu/cnm/deepdive/animals/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/animals/MainActivity.java
@@ -1,7 +1,13 @@
 package edu.cnm.deepdive.animals;
 
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import edu.cnm.deepdive.animals.model.Animal;
+import edu.cnm.deepdive.animals.service.WebServiceProxy;
+import java.io.IOException;
+import java.util.List;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+  }
+
+  private class Retriever extends Thread {
+
+    @Override
+    public void run() {
+
+      try {
+        Response<List<Animal>> response = WebServiceProxy.getInstance()
+            .getAnimals()
+            .execute();
+        if (response.isSuccessful()) {
+          Log.d(getClass().getName(), response.body().toString());
+        } else {
+          Log.e(getClass().getName(), response.message());
+        }
+      } catch (IOException e) {
+        Log.e(getClass().getName(), e.getMessage(), e);
+
+      }
+
+
+    }
   }
 }
